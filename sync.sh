463eeb8ca13646bfcfe0e6b0c332fe8d43e7aa24
Edvinas Danevičius <edvinas108@gmail.com>
2025-11-05 20:39:12 +0200

    Update sync script to allow overrides

--- sync.sh
+++ sync.sh
@@ -7,27 +7,41 @@ rcloneSync() {
   local fromPath="$1"
   local toPath="$2"
 
-  echo "Sync from: ${fromPath}, to: ${toPath}"
   if [ "${COMMAND_FLAG}" = '--apply' ]; then
+    echo "Sync: '${fromPath}' -> '${toPath}'"
     rclone sync --progress "${fromPath}" "${toPath}" --exclude desktop.ini
   else
+    echo "Sync (dry-run): '${fromPath}' -> '${toPath}'"
     rclone sync --dry-run "${fromPath}" "${toPath}" --exclude desktop.ini
   fi
 }
 
 pull() {
   while IFS='' read -r path; do
-    [[ "${path}" =~ ^#.*$ ]] && continue
+    [[ "${path}" =~ ^#.*$ || -z "${path}" ]] && continue
 
-    rcloneSync "remote:${path}" "${SYNC_DIR}/${path}"
+    IFS=':' read -r remotePath localPath <<<"${path}"
+    local localPath="${localPath/#\~/$HOME}"
+    local localPath="${localPath:-${SYNC_DIR}/${remotePath}}"
+
+    rcloneSync "remote:${remotePath}" "${localPath}"
   done <"${SYNC_PATHS}"
 }
 
 push() {
   while IFS='' read -r path; do
-    [[ "${path}" =~ ^#.*$ ]] && continue
+    [[ "${path}" =~ ^#.*$ || -z "${path}" ]] && continue
 
-    rcloneSync "${SYNC_DIR}/${path}" "remote:${path}"
+    IFS=':' read -r remotePath localPath <<<"${path}"
+    local localPath="${localPath/#\~/$HOME}"
+    local localPath="${localPath:-${SYNC_DIR}/${remotePath}}"
+
+    if [ ! -e "${localPath}" ]; then
+      echo "Skipping missing path: '${localPath}'"
+      continue
+    fi
+
+    rcloneSync "${localPath}" "remote:${remotePath}"
   done <"${SYNC_PATHS}"
 }
 
